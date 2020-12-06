58e9ef99f9e30022af1521c5c2df395c439e40c5
VictorHugo <victor.rocha93@outlook.com>
2020-12-06 01:13:31 +0100

    Another medium

---
+++ EdabitChallenges/Medium/MaskifyString.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+namespace EdabitChallenges.Medium
+{
+    public class MaskifyString
+    {
+        private const string CHAR_MASKED = "#";
+        public static string MaskifyStringMethod(string str)
+        {
+            var stringLength = str.Length;
+            if (stringLength < 4)
+                return str;
+
+            var lastFour = str[Math.Max(0, str.Length - 4)..];
+            return new string(string.Concat(Enumerable.Repeat(CHAR_MASKED, stringLength - 4)) + lastFour);
+        }
+    }
+}
--- UnitTests/Medium/MediumUnitTests.cs
+++ UnitTests/Medium/MediumUnitTests.cs
@@ -16,6 +16,20 @@ namespace UnitTests.Medium
         [TestCase("sPoNtAnEoUs", ExpectedResult = "SpOnTaNeOuS")]
         [TestCase("eXCELLENT, yOuR mAJESTY", ExpectedResult = "Excellent, YoUr Majesty")]
         public static string ReverseTheCaseTests(string str) => ReserveTheCase.ReserveTheCaseMethod(str);
-        
+
+        [Test]
+        [TestCase("4556364607935616", ExpectedResult = "############5616")]
+        [TestCase("64607935616", ExpectedResult = "#######5616")]
+        [TestCase("1", ExpectedResult = "1")]
+        [TestCase("", ExpectedResult = "")]
+        [TestCase("tBy>L/cMe+?<j:6n;C~H", ExpectedResult = "################;C~H")]
+        [TestCase("12", ExpectedResult = "12")]
+        [TestCase("8Ikhlf6yoxPOwi5cB014eWbRumj7vJ", ExpectedResult = "##########################j7vJ")]
+        [TestCase("123", ExpectedResult = "123")]
+        [TestCase(")E$aCU=e\"_", ExpectedResult = "######=e\"_")]
+        [TestCase("2673951408", ExpectedResult = "######1408")]
+        [TestCase("1234", ExpectedResult = "1234")]
+        public static string MaskifyStringTests(string str) => MaskifyString.MaskifyStringMethod(str);
+
     }
 }
